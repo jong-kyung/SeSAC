96035cf3ab74a0f76e27383dc69e685011cf441f
이종경 <gyoung3063413@naver.com>
2023-06-19 14:52:47 +0900

    [추가] convert 예제 파일 생성

--- convert.py
+++ convert.py
@@ -1,4 +1,17 @@
+# 미션1. 사용자로부터 문장을 입력받아 대문자로 변환하시오.
+# 미션2. 입력받은 문장에서 대문자는 소문자로, 소문자는 대문자로 변환하시오
 def convert_case(text):
-    print(text.upper())
+    result = ''
+    for i in text:
+        if i.isupper():
+            result += i.lower()
+        elif i.islower():
+            result += i.upper()
+        else:
+            result += i
+    
+    return result       
 
-convert_case('text')
+text= input('문자열을 입력하세요')
+result = convert_case(text)
+print('변환된 문장:',result)
