964a4adaca800fb6247af697b783f2b2c97d78bc
이종경 <gyoung3063413@naver.com>
2023-06-20 16:20:14 +0900

    [추가] reverse_text.py 주석추가

--- reverse_text.py
+++ reverse_text.py
@@ -1,5 +1,5 @@
 def palindrome(str):
-    reversed_str = str[::-1]
+    reversed_str = str[::-1] # 인덱싱에 ::-1 을하면 역순으로 복사됨
     if str.lower().replace(' ','') == reversed_str.lower().replace(' ',''):
         return True
     
