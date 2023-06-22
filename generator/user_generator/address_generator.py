074f6b9b06a75456bcecd24f48faca0595afd3f6
이종경 <gyoung3063413@naver.com>
2023-06-22 11:17:23 +0900

    [수정] user_generator 보완 및 기능 개선

--- generator/user_generator/address_generator.py
+++ generator/user_generator/address_generator.py
@@ -2,9 +2,19 @@ import random
 
 class AddressGenerator:
     def __init__(self):
-        self.cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia']
+        self.cities = []
+        self.sub_address = []
+        with open('cities.txt') as file:
+            cities = file.readlines()
+        for city in cities:
+            self.cities.append(city.strip())
+        
+        with open('sub_address.txt') as file:
+            sub_address = file.readlines()
+        for sub in sub_address:
+            self.sub_address.append(sub.strip())
 
     def generate_address(self):
-        street = random.randint(1,100)
         city = random.choice(self.cities)
-        return f'{street} {city}'
+        street = random.choice(self.sub_address)
+        return f'{city} {street}'
--- generator/user_generator/name_generator.py
+++ generator/user_generator/name_generator.py
@@ -2,6 +2,19 @@ import random
 
 class NameGenerator:
     def __init__(self):
-        self.names = ['John', 'Jane', 'Michael', 'Emily', 'William', 'Olivia']
+        self.first_names = []
+        self.last_names = []
+        with open('first_names.txt') as file:
+            first_names = file.readlines()
+        for first_name in first_names:
+            self.first_names.append(first_name.strip())
+
+        with open('last_names.txt') as file:
+            last_names = file.readlines()
+        for last_name in last_names:
+            self.last_names.append(last_name.strip())
+        
+
     def generate_name(self):
-        return random.choice(self.names)
+        user_name = random.choice(self.last_names) + random.choice(self.first_names)
+        return user_name
