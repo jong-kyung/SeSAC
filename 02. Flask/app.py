e497d826449d171d0705a1d5eaf023e011446894
이종경 <gyoung3063413@naver.com>
2023-07-03 10:19:35 +0900

    [추가] 각 페이지별 ui개선

--- 02. Flask/app.py
+++ 02. Flask/app.py
@@ -13,7 +13,7 @@ def root():
 def user():
     page = request.args.get('page', default=1, type=int) 
     search_name = request.args.get('name', default='', type=str)
-    gender = request.args.get('gender', default='', type=str)
+    gender = request.args.get('sub-data', default='', type=str)
     
     per_page = 35 # 내가 보여줄 페이지 갯수
     headers= [] # 맨 위의 헤딩 저장
@@ -43,7 +43,7 @@ def user():
         end_page = min(start_page + 4, total_range) # 전체페이지와 마지막 페이지를 비교하여 더 작은 값 선택
         
         result_datas = datas[start_index:end_index]
-        
+
         return render_template('list.html', dataname='user', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name, gender=gender, start_page = start_page, end_page = end_page)
     # """ """ 는 자바스크립트에서의 백틱과 유사함.
 
@@ -62,29 +62,37 @@ def user_info(param):
 def store():
     page = request.args.get('page', default=1, type=int) 
     search_name = request.args.get('name', default='', type=str)
-    
-    per_page = 35
-    headers= []
-    datas = []
-    result_datas = []
+    store_type = request.args.get('sub-data', default='', type=str)
+
+    per_page = 35 
+    headers= [] 
+    datas = [] 
+    result_datas = [] 
+    filter_datas = []
     
     with open('./crm/store.csv', 'r') as file:
         csv_data = csv.reader(file)
-        headers = next(csv_data) # 첫번째 줄 넣기
+        headers = next(csv_data) 
         for row in csv_data:
             if search_name in row[1]:
                 datas.append(row)
+           
+        for data in datas: 
+            if store_type in data:
+                filter_datas.append(data) 
+                datas = filter_datas
                 
-        total_len = len(datas) - 1 # header 제외
-        total_range = math.ceil(total_len // per_page)
-
-        start_index = (page - 1) * per_page
+        total_len = len(datas) - 1 
+        total_range = math.ceil(total_len // per_page) 
+        start_index = (page - 1) * per_page 
         end_index = start_index + per_page
-        min_page = min(1, start_index + 1)
 
+        start_page = ((page - 1) // 5)*5 + 1 #
+        end_page = min(start_page + 4, total_range) 
+        
         result_datas = datas[start_index:end_index]
-    
-        return render_template('list.html', dataname='store', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name, min_page = min_page)
+        
+        return render_template('list.html', dataname='store', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name, start_page = start_page, end_page = end_page)
 
 @app.route('/store/<param>')
 def store_info(param):
@@ -98,30 +106,40 @@ def store_info(param):
                 return render_template('detail.html', datas=findData)
             
 @app.route('/item')
-def item(page=1):
+def item():
     page = request.args.get('page', default=1, type=int) 
     search_name = request.args.get('name', default='', type=str)
-    
-    per_page = 35
-    headers= []
-    datas = []
-    result_datas = []
+    item_type = request.args.get('sub-data', default='', type=str)
+
+    per_page = 35 
+    headers= [] 
+    datas = [] 
+    result_datas = [] 
+    filter_datas = []
     
     with open('./crm/item.csv', 'r') as file:
         csv_data = csv.reader(file)
-        headers = next(csv_data) # 첫번째 줄 넣기
+        headers = next(csv_data) 
         for row in csv_data:
             if search_name in row[1]:
                 datas.append(row)
-                
-        total_len = len(datas) - 1 # header 제외
-        total_range = math.ceil(total_len // per_page)
+        
+        for data in datas: 
+            if item_type in data:
+                filter_datas.append(data) 
+                datas = filter_datas
 
-        start_index = (page - 1) * per_page
+        total_len = len(datas) - 1 
+        total_range = math.ceil(total_len // per_page) 
+        start_index = (page - 1) * per_page 
         end_index = start_index + per_page
+
+        start_page = ((page - 1) // 5)*5 + 1 #
+        end_page = min(start_page + 4, total_range) 
+        
         result_datas = datas[start_index:end_index]
-    
-        return render_template('list.html', dataname='item', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name)
+        
+        return render_template('list.html', dataname='item', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name, start_page = start_page, end_page = end_page)
     
 @app.route('/item/<param>')
 def item_info(param):
@@ -135,7 +153,7 @@ def item_info(param):
                 return render_template('detail.html', datas=findData)    
             
 @app.route('/order')
-def order(page=1):
+def order():
     page = request.args.get('page', default=1, type=int) 
     search_name = request.args.get('name', default='', type=str)
     
@@ -151,21 +169,24 @@ def order(page=1):
             if search_name in row[1]:
                 datas.append(row)
                    
-        total_len = len(datas) - 1 # header 제외
-        total_range = math.ceil(total_len // per_page)
-
-        start_index = (page - 1) * per_page
+        total_len = len(datas) - 1 
+        total_range = math.ceil(total_len // per_page) 
+        start_index = (page - 1) * per_page 
         end_index = start_index + per_page
+
+        start_page = ((page - 1) // 5)*5 + 1 #
+        end_page = min(start_page + 4, total_range) 
+        
         result_datas = datas[start_index:end_index]
-    
-        return render_template('list.html', dataname='order', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name)
+        
+        return render_template('list.html', dataname='order', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name, start_page = start_page, end_page = end_page)
     
 @app.route('/order/<param>')
 def order_info(param):
     findData = []
     with open('./crm/order.csv', 'r', newline='') as file:
         csv_data = csv.reader(file)
-        findData.append(next(csv_data)) # 첫번째 줄 넣기
+        findData.append(next(csv_data))
         for row in csv_data:
             if param in row:
                 findData.append(row)
@@ -188,14 +209,17 @@ def ordereditem(page=1):
             if search_name in row[1]:
                 datas.append(row)
                 
-        total_len = len(datas) - 1 # header 제외
-        total_range = math.ceil(total_len // per_page)
-
-        start_index = (page - 1) * per_page
+        total_len = len(datas) - 1 
+        total_range = math.ceil(total_len // per_page) 
+        start_index = (page - 1) * per_page 
         end_index = start_index + per_page
+
+        start_page = ((page - 1) // 5)*5 + 1 
+        end_page = min(start_page + 4, total_range) 
+        
         result_datas = datas[start_index:end_index]
-    
-        return render_template('list.html', dataname='ordereditem', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name)
+        
+        return render_template('list.html', dataname='ordereditem', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name, start_page = start_page, end_page = end_page)
     
 @app.route('/ordereditem/<param>')
 def orderitem_info(param):
