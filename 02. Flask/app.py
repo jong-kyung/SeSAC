e54bdca26948781996b657fc92da762c15c578ee
이종경 <gyoung3063413@naver.com>
2023-07-03 12:26:54 +0900

    [추가] 로그인 및 회원가입 기능 추가

--- 02. Flask/app.py
+++ 02. Flask/app.py
@@ -1,51 +1,103 @@
-from flask import Flask, url_for, redirect, render_template, request
+from flask import Flask, url_for, redirect, render_template, request, jsonify, make_response
 import csv
 import math
+import datetime 
+import jwt # JWT 토큰 생성용 라이브러리
+import hashlib # 해쉬암호화 라이브러리
+SECRET_KEY = 'SESAC'
 
 app = Flask(__name__) # 고유식별자를 넣어주어야함 보통 __name__으로 작명함
 
+@app.route('/sign', methods=['POST'])
+def sign_up():
+    sign_id = request.form['sign_id']
+    sign_pw = request.form['sign_pw']
+
+    pw_hash = hashlib.sha256(sign_pw.encode('utf-8')).hexdigest() # 해쉬암호화
+
+    save_data = {'id': sign_id,'pw': pw_hash}
+    with open('./crm/login.csv', 'w') as file:
+        csv_data = csv.writer(file)
+        for key, value in save_data.items():
+            csv_data.writerow([key, value])
+    
+    return redirect('/')
+
+@app.route('/login', methods=['POST'])
+def login():
+    id = request.form['id']
+    pw = request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    result = []
+
+    with open('./crm/login.csv', 'r') as file:
+        csv_data = csv.reader(file)
+        for row in csv_data:
+            if row[1] == id:
+                result.append(row[1])
+                continue
+            if row[1] == pw_hash:
+                result.append(row[1])
+    if result != []:
+        payload = {
+            'id': id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60*60)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        response = make_response(redirect(url_for('user'))) # 응답객체 생성
+        
+        response.set_cookie('token', token, httponly=True) # 토큰을 쿠키에 저장
+        return response
+    else:
+        return jsonify({'result':'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다'})
+
 @app.route('/')
 def root():
-    return redirect(url_for('user'))
+        return render_template('login.html')
 
 # 연산은 백엔드에서 처리하는게 가장 좋음, 연산이 적은거는 프론트에서
 @app.route('/user')
 def user():
-    page = request.args.get('page', default=1, type=int) 
-    search_name = request.args.get('name', default='', type=str)
-    gender = request.args.get('sub-data', default='', type=str)
-    
-    per_page = 35 # 내가 보여줄 페이지 갯수
-    headers= [] # 맨 위의 헤딩 저장
-    datas = [] # 데이터 담을 list
-    result_datas = [] # 데이터 쪼개서 보여줄때 넣어주기
-    filter_datas = []
-    
-    with open('./crm/user.csv', 'r') as file:
-        csv_data = csv.reader(file)
-        headers = next(csv_data) # 첫번째 줄 넣기
-        for row in csv_data:
-            # TODO : 어떻게 코드를 합칠 수 있을까? 따로하면 작성은 됨..
-            if search_name in row[1]:
-                datas.append(row) # csv 데이터 삽입
-
-        for data in datas: # 데이터들을 for문을 통해 필터링 작업
-            if gender in data:
-                filter_datas.append(data) 
-                datas = filter_datas
-            
-        total_len = len(datas) - 1 # header 제외
-        total_range = math.ceil(total_len // per_page) # 페이지네이션 갯수
-        start_index = (page - 1) * per_page 
-        end_index = start_index + per_page
-
-        start_page = ((page - 1) // 5)*5 + 1 # 5개 단위로 끊기
-        end_page = min(start_page + 4, total_range) # 전체페이지와 마지막 페이지를 비교하여 더 작은 값 선택
+    token = request.cookies.get('token') # 토큰 받아오기
+    try: 
+        page = request.args.get('page', default=1, type=int) 
+        search_name = request.args.get('name', default='', type=str)
+        gender = request.args.get('sub-data', default='', type=str)
         
-        result_datas = datas[start_index:end_index]
+        per_page = 35 # 내가 보여줄 페이지 갯수
+        headers= [] # 맨 위의 헤딩 저장
+        datas = [] # 데이터 담을 list
+        result_datas = [] # 데이터 쪼개서 보여줄때 넣어주기
+        filter_datas = []
+        
+        with open('./crm/user.csv', 'r') as file:
+            csv_data = csv.reader(file)
+            headers = next(csv_data) # 첫번째 줄 넣기
+            for row in csv_data:
+                if search_name in row[1]:
+                    datas.append(row) # csv 데이터 삽입
 
-        return render_template('list.html', dataname='user', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name, gender=gender, start_page = start_page, end_page = end_page)
-    # """ """ 는 자바스크립트에서의 백틱과 유사함.
+            for data in datas: # 데이터들을 for문을 통해 필터링 작업
+                if gender in data:
+                    filter_datas.append(data) 
+                    datas = filter_datas
+                
+            total_len = len(datas) - 1 # header 제외
+            total_range = math.ceil(total_len // per_page) # 페이지네이션 갯수
+            start_index = (page - 1) * per_page 
+            end_index = start_index + per_page
+
+            start_page = ((page - 1) // 5)*5 + 1 # 5개 단위로 끊기
+            end_page = min(start_page + 4, total_range) # 전체페이지와 마지막 페이지를 비교하여 더 작은 값 선택
+            
+            result_datas = datas[start_index:end_index]
+
+            return render_template('list.html', dataname='user', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name, gender=gender, start_page = start_page, end_page = end_page)
+        # """ """ 는 자바스크립트에서의 백틱과 유사함.
+    except jwt.ExpiredSignatureError:
+        return '토큰이 만료되었습니다'
+    except jwt.InvalidTokenError:
+        return '유효하지 않은 토큰입니다'
 
 @app.route('/user/<param>')
 def user_info(param):
