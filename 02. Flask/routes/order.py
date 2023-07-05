4a967004eaea6d6d74bd77c65c21b46f18deaa9e
이종경 <gyoung3063413@naver.com>
2023-07-05 12:18:57 +0900

    [수정] 모듈 생성을 통한 리펙토링

--- 02. Flask/routes/order.py
+++ 02. Flask/routes/order.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, render_template
 import csv
-import math
+from common.functions import parse_data
 from common.verify import check_login
 order = Blueprint('order', __name__)
 
@@ -8,32 +8,9 @@ order = Blueprint('order', __name__)
 @order.route('/order')
 @check_login
 def order_list():
-    page = request.args.get('page', default=1, type=int) 
-    search_name = request.args.get('name', default='', type=str)
-    
-    per_page = 35
-    headers= []
-    datas = []
-    result_datas = []
-    
-    with open('./crm/order.csv', 'r') as file:
-        csv_data = csv.reader(file)
-        headers = next(csv_data) # 첫번째 줄 넣기
-        for row in csv_data:
-            if search_name in row[1]:
-                datas.append(row)
-                   
-        total_len = len(datas) - 1 
-        total_range = math.ceil(total_len // per_page) 
-        start_index = (page - 1) * per_page 
-        end_index = start_index + per_page
+    data = parse_data('order', 10)
 
-        start_page = ((page - 1) // 5)*5 + 1 #
-        end_page = min(start_page + 4, total_range) 
-        
-        result_datas = datas[start_index:end_index]
-        
-        return render_template('list.html', dataname='order', headers = headers, datas = result_datas, total_range = total_range, page = page, search_name = search_name, start_page = start_page, end_page = end_page)
+    return render_template('list.html', dataname='order', headers = data['headers'], datas = data['result_datas'], total_range = data['total_range'], page = data['page'], search_name = data['search_name'], gender=data['sub_data'], start_page = data['start_page'], end_page = data['end_page'])
     
 @order.route('/order/<param>')
 @check_login
@@ -45,5 +22,5 @@ def order_info(param):
         for row in csv_data:
             if param in row:
                 findData.append(row)
-                return render_template('detail.html', datas=findData) 
-                   
+    return render_template('detail.html', datas=findData) 
+                
