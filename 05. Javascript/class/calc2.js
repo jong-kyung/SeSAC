198393ff2cc91fca3a4eec7be42dcdcc589d562d
이종경 <gyoung3063413@naver.com>
2023-07-11 17:20:48 +0900

    [수정] 폴더명 변경

--- 05. Javascript/class/calc2.js
+++ 05. Javascript/class/calc2.js
@@ -7,14 +7,13 @@ let number = '';
 buttons.forEach((a, i) => {
     a.addEventListener('click', (e) => {
         let _this = e.currentTarget;
-        if (_this.innerHTML == 'C') {
-            /* 사용자의 UI 초기화 및 입력중이었던 값 초기화 */
-            number = '';
+        if (operator.includes(_this.innerHTML)) {
+            input.value = '';
+        } else if (_this.innerHTML == 'C') {
+            /* 사용자의 UI 초기화 */
             input.value = '';
             result = 0;
         } else if (_this.innerHTML == '=') {
-        
-
             input.value = result
         } else { // 숫자를 눌렀을 경우
             number += _this.innerHTML
