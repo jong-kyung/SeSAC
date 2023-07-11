ed5e6c28a4c73baae2247a070a32dd8bf10953b2
이종경 <gyoung3063413@naver.com>
2023-07-11 23:04:16 +0900

    [삭제] 프로젝트 파일 분리

--- selenium/trans_file/trans_to_csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def SaveToCSV(FileName, Chart):
-    with open(f'./chart_list/{FileName}.csv', 'w', newline='') as file:
-        csv_data = csv.DictWriter(file, fieldnames= ['Title', 'Artist', 'AlbumTitle'])
-        csv_data.writeheader()
-        csv_data.writerows(Chart)
