beed74237b3e0a01ae312998ec1cff7612fc376b
이종경 <gyoung3063413@naver.com>
2023-07-12 16:37:13 +0900

    [추가] DB 와 chart.js 연동하기

--- 05. Javascript/charts/crm/app.py
+++ 05. Javascript/charts/crm/app.py
@@ -1,13 +1,26 @@
 from flask import Flask, render_template
 import sqlite3
 
-app=Flask(__name__)
+app=Flask(__name__) 
+print('hello')
 
 @app.route('/')
 def index():
     # TODO : 월간 매출액 합산을 구하시오
-    
-    return render_template('index.html', rows=rows)
+    conn = sqlite3.connect('./DB/crm.db')
 
-if __name__ == 'main':
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT substr(orders.OrderAt,1, 4) AS 'Year', substr(orders.OrderAt, 6, 2) AS 'Month', stores.name, SUM(items.UnitPrice) AS 'Total' FROM orders INNER JOIN stores ON orders.storeId = stores.Id INNER JOIN orderitems ON  orders.Id = orderitems.Orderid INNER JOIN items ON orderitems.ItemId = items.Id WHERE stores.name = '커피빈 홍대6호점' GROUP BY Year, Month")
+    rows = cursor.fetchall()
+    
+    labels = []
+    revenues = []
+    for row in rows:
+        labels.append(row[1])
+        revenues.append(row[3])
+    conn.close()
+    return render_template('index.html', rows=rows, labels = labels, revenues = revenues)
+
+if __name__ == '__main__':
     app.run(debug=True, port=8080)
