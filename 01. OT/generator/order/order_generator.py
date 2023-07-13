60d2f65ee064ec72b0dad4f4f73a27756bdafca7
이종경 <gyoung3063413@naver.com>
2023-07-13 16:27:12 +0900

    [수정] order-generator 버그 수정

--- 01. OT/generator/order/order_generator.py
+++ 01. OT/generator/order/order_generator.py
@@ -2,20 +2,24 @@ from common.generator import Generator
 from models.order import Order
 from common.find_row import find_row
 
-import calendar
+from datetime import datetime, timedelta
 import random
 import uuid
 
 class OrderGenerator(Generator):
     def __init__(self):
-        self.year = random.randint(2020, 2023) 
-        self.month = random.randint(1,12)
         self.StoreId = find_row('store','Id')
         self.UserId = find_row('user', 'Id')
 
     def generator(self):
         order_id = uuid.uuid4()
-        OrderAt = random.randint(1,calendar.monthrange(self.year, self.month)[1])
+        start = datetime(2023, 1, 1)  # 범위의 시작 날짜
+        end = datetime(2023, 12, 31)  # 범위의 끝 날짜
+
+        # 범위 내에서 랜덤한 날짜 생성
+        time_difference = end - start
+        random_seconds = random.randint(0, time_difference.total_seconds())
+        OrderAt = start + timedelta(seconds=random_seconds)
         StoreId = random.choice(self.StoreId)
         UserId = random.choice(self.UserId)
 
