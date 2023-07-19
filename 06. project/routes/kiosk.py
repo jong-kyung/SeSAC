676a6915aa00c58db27b6b7cb4d734d9be66dc91
이종경 <gyoung3063413@naver.com>
2023-07-19 15:37:22 +0900

    [추가] KIOSK 메인 UI 생성

--- 06. project/routes/kiosk.py
+++ 06. project/routes/kiosk.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for, make_response, request
+from flask import Blueprint, render_template, redirect, url_for, make_response, jsonify, request
 
 import math
+import json
 
 from common.verify import check_user
 from sql.store_query import Store_query
@@ -16,7 +17,7 @@ def store_ui():
     search_name = request.args.get('name', default='', type=str)
     sub_data = request.args.get('sub_data', default='', type=str)
     current_url = request.path
-    print(current_url)
+
     try:
         per_page = 12
   
@@ -49,45 +50,48 @@ def store_ui():
     except TypeError:
         return redirect('kiosk', next='/1')
     
-@kiosk.route('/kiosk/<store_id>')
+@kiosk.route('/kiosk/item', methods=['GET', 'POST'])
 @check_user
-def item_ui(store_id):
-    page = request.args.get('page', default=1, type=int) 
-    search_name = request.args.get('name', default='', type=str)
-    sub_data = request.args.get('sub_data', default='', type=str)
-    current_url = request.path
+def item_ui():
+    if request.method == 'POST':
+        payload = request.get_json()
+        response = make_response(redirect(url_for('kiosk.item_ui')))
+        response.set_cookie('store_id', json.dumps(payload), httponly=True)
 
-    try:
+        return response
+    else:
+        search_name = request.args.get('name', default='', type=str)
+        sub_data = request.args.get('sub_data', default='', type=str)
+
         per_page = 15
 
         items = Item_query('crm', 'items')
         headers = items.schema_query() # schema 받아오기
         item_types = items.find_data_query('Type') # Type 받아오기
         result_datas = [] # 결과 데이터 삽입용
-        datas = items.total_data_query(page, per_page, 'Name', search_name, 'Type', sub_data)
+        datas = items.total_data_query(1, per_page, 'Name', search_name, 'Type', sub_data)
 
-        # -------- 페이지네이션 --------
-        total_data_len = datas['data_length'] # 데이터 전체 갯수
-        page_range = math.ceil(total_data_len/per_page) # 페이지 갯수 구하기
             # ---- 데이터 자르기 ----
         result_datas = datas['datas'] # 데이터 자르기
         
-        # 의도치 않은 페이지 이동시 예외처리
-        if page < 1:
-            page = 1
-            return redirect(url_for('kiosk.item_ui'))
-        elif page > page_range:
-            page = page_range
-            return redirect(url_for('kiosk.item_ui'))
 
-    # 페이지네이션
-        start_page = page - (page-1) % 5 # 5개 단위로 끊기
-        end_page = min(start_page + 4, page_range) # 끝페이지 정해주기
+        return render_template('kiosk/item_list.html', search_name = search_name, sub_data = sub_data, types = item_types, headers = headers, datas = result_datas)
+
+@kiosk.route('/kiosk/result', methods=['GET','POST'])
+@check_user
+def view_result():
+    if request.method == 'POST':
+        payload = request.get_json()
+        response = make_response(redirect(url_for('kiosk.view_result')))
+        response.set_cookie('item_info', json.dumps(payload))
+
+        return response
+    else :
+        item_info = json.loads(request.cookies.get('item_info'))
+        store_id = json.loads(request.cookies.get('store_id'))
+
         
-        return render_template('kiosk/item_list.html', search_name = search_name, sub_data = sub_data, page = page, types = item_types, headers = headers, datas = result_datas, page_range = page_range, start_page = start_page, end_page = end_page, current_url = current_url)
-        
-    except TypeError:
-        return redirect('kiosk', next='/1')
+        return render_template('kiosk/kiosk_result.html',item_info = item_info)
 
 @kiosk.route('/kiosk/logout')
 @check_user
@@ -96,4 +100,5 @@ def logout():
     response.delete_cookie('token')
     
     return response
+    
 
--- 06. project/sql/kiosk_query.py
+++ 06. project/sql/kiosk_query.py
@@ -4,13 +4,6 @@ class Kiosk_query(SQLite3_connect):
     def __init__(self, DB_Name):
         super().__init__(DB_Name)
 
-    def result_query(self, store_id, item_id):
-        self.cursor.execute(f"SELECT Name, Price FROM items WHERE Id = ?", (item_id,))
-        item_info = self.cursor.fetchall()
-        
-        self.cursor.execute(f"SELECT Name FROM stores WHERE Id = ?",(store_id,))
-        store_info = self.cursor.fetchall()
-        
-        result = item_info + store_info
-
-        return result
+    def result_query(self, data):
+        print(data)
+        self.cursor.execute()
