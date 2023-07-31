6beb0e1c5706900fa68af1e37d26f4d0587d2cf7
이종경 <gyoung3063413@naver.com>
2023-07-31 14:51:55 +0900

    [추가] Django  로그인 미들웨어 추가

--- 14. Django/HelloWorldProject/helloworldapp/urls.py
+++ 14. Django/HelloWorldProject/helloworldapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
+    path('home/', views.home_view, name='login_user_home'),
     path('login/', views.login_view, name = 'login_view'),
-    path('home/', views.home_view, name='login_user_home')
+    path('logout/', views.logout_view, name='logout')
 ]
--- 14. Django/HelloWorldProject/helloworldapp/views.py
+++ 14. Django/HelloWorldProject/helloworldapp/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate
 # Create your views here.
 
 def hello_world(request):
@@ -13,6 +14,7 @@ def login_view(request):
         user = authenticate(request, username = username, password = password)
         if user:
             print('로그인 성공')
+            login(request,user) # login 정보 저장
             return redirect('login_user_home')
         else:
             print('로그인 실패')
@@ -20,4 +22,17 @@ def login_view(request):
     return render(request, 'login.html')
 
 def home_view(request):
-    return render(request, 'home.html')
+    # 로그인한 사용자 정보
+    user = request.user
+    context = { # 여러개의 데이터를 바인딩하는 방법
+        'username':user.username,
+        'first_name':user.first_name,
+        'last_name':user.last_name,
+        'email':user.email
+    }
+
+    return render(request, 'home.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('login_view')
