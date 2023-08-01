d3cdc88d8ce138bb2781ee59faf61cbbcb4a13f3
이종경 <gyoung3063413@naver.com>
2023-08-01 16:14:18 +0900

    [추가] Admin페이지에 Todo 객체 등록

--- 14. Django/HelloWorld2/todo/models.py
+++ 14. Django/HelloWorld2/todo/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
