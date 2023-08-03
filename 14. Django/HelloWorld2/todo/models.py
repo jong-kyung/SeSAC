2ec67de00a155def610ef8c58bb7c6001b3a901b
이종경 <gyoung3063413@naver.com>
2023-08-03 12:57:56 +0900

    [추가] Django Todo Test기능 추가

--- 14. Django/HelloWorld2/todo/models.py
+++ 14. Django/HelloWorld2/todo/models.py
@@ -5,3 +5,6 @@ class Todo(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self) -> str:
+        return self.title
--- 14. Django/HelloWorld2/todo/urls.py
+++ 14. Django/HelloWorld2/todo/urls.py
@@ -3,8 +3,8 @@ from todo import views
 
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
-    path('write/', views.todo_write, name='todo_wirte'),
+    path('write/', views.todo_write, name='todo_write'),
     path('<int:pk>/', views.todo_detail, name='todo_detail'),
-    path('<int:pk>/delete/', views.del_todo, name='dle_todo'),
-    # path('<int:pk>/update/', views.update_todo, name='update_todo')
+    path('<int:pk>/delete/', views.del_todo, name='del_todo'),
+    path('<int:pk>/update/', views.update_todo, name='update_todo')
 ]
