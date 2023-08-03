2cfff1bb716272a2a41927f478a46c1e6cdac239
이종경 <gyoung3063413@naver.com>
2023-08-03 15:58:31 +0900

    [추가] Django 투표 결과UI 추가

--- 14. Django/HelloWorld2/vote/urls.py
+++ 14. Django/HelloWorld2/vote/urls.py
@@ -4,6 +4,5 @@ from vote import views
 urlpatterns = [
     path('', views.question_list, name='question_list'),
     path('<int:pk>', views.choice_list, name='choice_list'),
-    # path('<int:question_id>/results/', views.results, name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),  
+    path('<int:pk>/result/', views.result_choice, name='result_choice'),
 ]
--- 14. Django/HelloWorld2/vote/views.py
+++ 14. Django/HelloWorld2/vote/views.py
@@ -10,12 +10,19 @@ def question_list(request):
 def choice_list(request, pk):
     question = get_object_or_404(Question, pk=pk)
     if request.method == 'POST':
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice']) # question, choice join문법, choice의 fk와 question의 pk가 일치하는 값
         selected_choice.count += 1
         selected_choice.save()
-
-        return redirect('question_list')
+        print(f'선택지:{selected_choice.Question.pk}')
+        return redirect('result_choice', selected_choice.Question.pk)
     else:
         selected_choice = question.choice_set.all()
-        print(selected_choice)
         return render(request, 'choice.html', {'questions':selected_choice})
+    
+def result_choice(request, pk):
+    choice = Choice.objects.filter(Question = pk).all()
+    print(choice)
+
+    return render(request, 'result.html', {
+        'choices' : choice
+    })
